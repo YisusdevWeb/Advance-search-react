b94f6b08c367b2373e05812a727e2ee6bee8861d
Yisus_dev <jesusdavid0518@hotmail.com>
2023-09-29 21:53:41 -0500

    buscador

---
+++ src/components/Search.jsx
@@ -0,0 +1,23 @@
+export default function Search() {
+return (
+
+<form class="relative h-10 w-72 min-w-[200px]">
+<div className="flex relative p-1  bg-gradient-to-tr from-purple-600 to-blue-300 rounded-full w-2/6">
+
+<input type="text" placeholder="Search Carro"  className='flex-1 p-2 pl-4 rounded-full w-full' />
+
+
+</div>
+
+
+
+
+</form>
+
+
+    
+)
+
+
+
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import Search from '../src/components/Search';
+import App from '../src/App.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
   <React.StrictMode>
-    <App />
+   <Search />
   </React.StrictMode>,
 )
